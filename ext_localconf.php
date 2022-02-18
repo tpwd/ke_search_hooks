8a29ebdd279fd87a36c9238d5277bbeedafdb22c
Christian Bülter <christian.buelter@web.de>
2022-02-18 13:34:47 +0100

    [TASK] Encapsulate code in ext_localconf.php in Closure

--- ext_localconf.php
+++ ext_localconf.php
@@ -20,42 +20,43 @@
 if (!defined("TYPO3")) {
     die("Access denied.");
 }
+(function () {
+    // Register custom indexer.
+    // Adjust this to your namespace and class name.
+    // Adjust the autoloading information in composer.json, too!
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerIndexerConfiguration'][] =
+        \Tpwd\KeSearchHooks\ExampleIndexer::class;
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['customIndexer'][] =
+        \Tpwd\KeSearchHooks\ExampleIndexer::class;
 
-// Register custom indexer.
-// Adjust this to your namespace and class name.
-// Adjust the autoloading information in composer.json, too!
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerIndexerConfiguration'][] =
-    \Tpwd\KeSearchHooks\ExampleIndexer::class;
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['customIndexer'][] =
-    \Tpwd\KeSearchHooks\ExampleIndexer::class;
+    // Register hooks for indexing additional fields.
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPageContentFields'][] =
+        \Tpwd\KeSearchHooks\AdditionalContentFields::class;
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyContentFromContentElement'][] =
+        \Tpwd\KeSearchHooks\AdditionalContentFields::class;
 
-// Register hooks for indexing additional fields.
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPageContentFields'][] =
-    \Tpwd\KeSearchHooks\AdditionalContentFields::class;
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyContentFromContentElement'][] =
-    \Tpwd\KeSearchHooks\AdditionalContentFields::class;
+    // Register hook to check if a content element should be indexed
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['contentElementShouldBeIndexed'][] =
+        \Tpwd\KeSearchHooks\AdditionalContentFields::class;
 
-// Register hook to check if a content element should be indexed
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['contentElementShouldBeIndexed'][] =
-    \Tpwd\KeSearchHooks\AdditionalContentFields::class;
+    // Register hook to add a custom autosuggest provider (ke_search_premium feature)
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search_premium']['modifyAutocompleWordList'][] =
+        \Tpwd\KeSearchHooks\AutosuggestProvider::class;
 
-// Register hook to add a custom autosuggest provider (ke_search_premium feature)
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search_premium']['modifyAutocompleWordList'][] =
-    \Tpwd\KeSearchHooks\AutosuggestProvider::class;
+    // Register hook to add custom values to the result row partial
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['additionalResultMarker'][] =
+        \Tpwd\KeSearchHooks\AdditionalResultMarker::class;
 
-// Register hook to add custom values to the result row partial
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['additionalResultMarker'][] =
-    \Tpwd\KeSearchHooks\AdditionalResultMarker::class;
+    // Register hook to change the sorting
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['getOrdering'][] =
+        \Tpwd\KeSearchHooks\Ordering::class;
 
-// Register hook to change the sorting
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['getOrdering'][] =
-    \Tpwd\KeSearchHooks\Ordering::class;
-
-// Register hook to register additional fields in the index table
-// Make sure to set the values for the additional fields in *every indexer* you use
-//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerAdditionalFields'][] =
+    // Register hook to register additional fields in the index table
+    // Make sure to set the values for the additional fields in *every indexer* you use
+    //$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerAdditionalFields'][] =
     //\Tpwd\KeSearchHooks\AdditionalIndexerFields::class;
 
-// Example for showing images of fe_users if you have implemented a fe_users indexer
-//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['fileReferenceTypes']['fe_users']['table'] = 'fe_users';
-//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['fileReferenceTypes']['fe_users']['field'] = 'image';
+    // Example for showing images of fe_users if you have implemented a fe_users indexer
+    //$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['fileReferenceTypes']['fe_users']['table'] = 'fe_users';
+    //$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['fileReferenceTypes']['fe_users']['field'] = 'image';
+})();
